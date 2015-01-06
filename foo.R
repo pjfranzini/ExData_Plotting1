132ea160d1f388e1da25151e814f02eeffa654fe
Paula Franzini <pjfranzini@hotmail.com>
2015-01-06 12:36:34 -0500

    png plots more or less done (504 vs 480 problem)

--- foo.R
+++ foo.R
@@ -13,7 +13,7 @@ png(file = "plot1.png", width = 480, height = 480, bg = "transparent")
 ## so exact correspondence is a bit confusing
 
 ##  cex.lab=0.75, cex.main=0.95, cex.axis = .75  -- not needed for png version
-par(mar = c(4.5, 4, 4, 1)) ## without this graph is a bit smaller than the one we are to match
+par(mar = c(4.5, 3.8, 3, 1)) ## without this graph is a bit smaller than the one we are to match
 hist(powerC$Global_active_power, col = "red",
      main="Global Active Power",
      ylab="Frequency", xlab="Global Active Power (kilowatts)",
@@ -24,41 +24,47 @@ axis(side = 2, at = c(0,200, 400, 600, 800, 1000, 1200))
 dev.off()
 
 ##plot2
+png(file = "plot2.png", width = 480, height = 480, bg = "transparent")
+par(mar = c(4.5, 3.8, 3, 1)) ## without this graph is a bit smaller than the one we are to match
 with(powerC,  plot(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Global_active_power,
-     type = "l", cex.lab=0.75, cex.axis = .75,
+     type = "l", 
      xlab="", ylab="Global Active Power (kilowatts)" ))
+dev.off()
 
 ##plot3
+png(file = "plot3.png", width = 480, height = 480, bg = "transparent")
+par(mar = c(4.5, 3.8, 3, 1)) ## without this graph is a bit smaller than the one we are to match
 with(powerC,  plot(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_1,
-                   type = "l", cex.lab=0.75, cex.axis = .75,
+                   type = "l",
                    xlab="", ylab="Energy sub metering" ))
 
 with(powerC,  points(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_2, col = "red", type = "l"))
 with(powerC,  points(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_3, col = "blue", type = "l"))
 
 legend("topright",  col = c("black", "red", "blue"), lwd = 1,
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       cex = .75)
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
 
 ##plot4
-par(mfrow = c(2, 2), mar = c(4, 1, 4, 0)) 
+png(file = "plot4.png", width = 504, height = 504, bg = "transparent")
+par(mfrow = c(2, 2), mar = c(4.3, 3.8, 3, 1))
 with(powerC, {
   plot(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Global_active_power,
-                     type = "l", cex.lab=0.75, cex.axis = .75,
+                     type = "l",
                      xlab="", ylab="Global Active Power" )
   plot(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Voltage,
-                     type = "l", cex.lab=0.75, cex.axis = .75,
+                     type = "l",
                      xlab="datetime", ylab="Voltage" )
   plot(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_1,
-                     type = "l", cex.lab=0.75, cex.axis = .75,
+                     type = "l",
                      xlab="", ylab="Energy sub metering" )
   points(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_2, col = "red", type = "l")
   points(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_3, col = "blue", type = "l")
   legend("topright",  col = c("black", "red", "blue"), lwd = 1,
          legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-         cex = .9, bty = "n")
+        bty = "n")
   plot(strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Global_reactive_power,
-       type = "l", cex.lab=0.75, cex.axis = .75,
+       type = "l",
        xlab="datetime", ylab="Global_reactive_power" )
-  
 })
+dev.off()
